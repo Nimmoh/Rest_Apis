0b19d46f504bedfa17462cd6e45b8a5c5278ec95
joy <nimmohndabari@gmail.com>
2024-01-24 18:39:23 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rest_api/screen/home.dart';
 
 
+
 void main(){
   runApp(MyApp());
 
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:rest_api/model/user.dart';
+import 'package:rest_api/model/user_name.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +14,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
+    
+
+    @override
+    void initState(){
+      super.initState();
+      fetchUsers();
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -23,31 +33,65 @@ class _HomeScreenState extends State<HomeScreen> {
         itemCount: users.length,
         itemBuilder: (context, index){
           final user = users[index];
-          final name = user['name'];
-          final email = user['email'];
+          final email = user.email;
+          final Color = user.gender == 'male' ? Colors.blue : Colors.green;
+          
         return ListTile(
-          leading: CircleAvatar(child:
-          Text('${index + 1}'),
-          ),
-          title: Text(name),
-          subtitle: Text(email),
+          title: Text(user.fullName),
+          subtitle:Text(user.phone),
+          //tileColor: Color,
         );
     },
       ),
-      floatingActionButton: FloatingActionButton(onPressed: fetchUsers,
-      ),
+      //floatingActionButton: FloatingActionButton(onPressed: fetchUsers,),
     );
+
+    }
+    Future<void> fetchUsers() async{
+      final response =await UserApi.fetchUsers();
+      setState(() {
+        users =response;
+      });
+    }
   }
-  void fetchUsers() async {
+  Future<List<void>> fetchUsers() async {
+    var logger = Logger();
+
     print("fetchUsers called");
     const url = 'https://randomuser.me/api/?results=5000';
     final uri = Uri.parse(url);
     final response =await http.get(uri);
     final body = response.body;
+
     final json = jsonDecode(body);
+     logger.t(json);
+
+    final results = json['results'] as List<dynamic>;
+    final Transformed = results.map((e){
+      final name = UserName(
+        title: e['user']['title'],
+        first: e['user']['first'], 
+        last: e['user']['last'], 
+      );
+        
+        return User(
+          cell: e['cell'],
+          email: e['email'],
+          gender: e['nat'],
+          nat: e['nat'],
+          phone: e['phone'],
+          name: name,
+        );
+      
+        
+      }).toList();
+      return Transformed;
+
+
+
+
     setState(() {
-      users = json['results'];
+      users = Transformed;
     });
-    print('fetch users completed');
+    log('fetch users completed' as num);
   }
-}
